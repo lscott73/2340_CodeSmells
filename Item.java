87dd6557e2e48d990e5a3d5d5d0e1686bced5e1d
rgseoo8 <rgseoo7@gmail.com>
2023-11-14 19:03:16 -0500

    item

--- Item.java
+++ Item.java
@@ -1,16 +1,14 @@
-class Item {
+public abstract class Item {
     private String name;
     private double price;
     private int quantity;
-    private DiscountType discountType;
-    private double discountAmount;
+    private DiscountType discount;
 
-    public Item(String name, double price, int quantity, DiscountType discountType, double discountAmount) {
+    public Item(String name, double price, int quantity, DiscountType discount) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
-        this.discountType = discountType;
-        this.discountAmount = discountAmount;
+        this.discount = discount;
     }
 
     public String getName() {
@@ -25,11 +23,9 @@ class Item {
         return quantity;
     }
 
-    public DiscountType getDiscountType() {
-        return discountType;
+    public DiscountType getDiscount() {
+        return discount;
     }
 
-    public double getDiscountAmount() {
-        return discountAmount;
-    }
+    public abstract double calculatePriceAfterDiscount();
 }
